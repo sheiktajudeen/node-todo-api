461e77bfde5900cf9fd46c6ff6087e2325179bd1
stajud001c <sheik_tajudeen@comcast.com>
2017-03-07 11:23:59 -0700

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./model/todo');
@@ -16,7 +17,6 @@ app.post('/todos',(req, res) => {
   todo.save().then((doc) => {
     res.send(doc);
   }, (e) => {
-
     res.status(400).send(e);
   })
 })
@@ -26,6 +26,21 @@ app.get('/todos', (req,res) => {
     res.send({todos});
   },(e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send({});
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send({});
+    }
+    res.status(200).send({todo});
+  }).catch((e) => {
+    res.status(400).send({});
   });
 });
 
