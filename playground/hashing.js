44fe7020d6ed6c54079b2d14bb6bf0c42cec6b69
stajud001c <sheik_tajudeen@comcast.com>
2017-03-08 11:40:29 -0700

    Hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,14 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err,hash) => {
+    console.log(hash);
+  })
+});
 //
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
