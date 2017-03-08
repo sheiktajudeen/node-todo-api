df62d66143a09234f813313bbc1794db950968d1
stajud001c <sheik_tajudeen@comcast.com>
2017-03-07 22:02:23 -0700

    Add POST /users route and token methods

---
+++ playground/hashing.js
@@ -0,0 +1,19 @@
+const {SHA256} = require('crypto-js')
+const jwt = require('jsonwebtoken');
+
+//
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+//
+// console.log(`message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token,'123abc');
+// console.log(decoded);
--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.post('/todos',(req, res) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 })
 
 app.get('/todos', (req,res) => {
@@ -83,6 +83,18 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+
+app.post('/users',(req, res) => {
+  var body = _.pick(req.body, ['email','password']);
+  var user = new User(body);
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+})
 
 app.listen(process.env.PORT,() => {
   console.log(`Started up on port ${process.env.PORT}`);
